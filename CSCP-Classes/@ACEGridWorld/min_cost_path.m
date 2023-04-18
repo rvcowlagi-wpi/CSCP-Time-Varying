872c08590ff08f23cad02a97b4c3867ad2ccbd36
rvcowlagi-wpi <129526829+rvcowlagi-wpi@users.noreply.github.com>
2023-04-18 13:07:19 -0400

    Updates to path planning, not yet complete

--- CSCP-Classes/@ACEGridWorld/min_cost_path.m
+++ CSCP-Classes/@ACEGridWorld/min_cost_path.m
@@ -3,8 +3,6 @@ SOFTWARE LICENSE
 ----------------
 Copyright (c) 2023 by 
 	Raghvendra V Cowlagi
-	Bejamin Cooper
-	Prakash Poudel
 
 Permission is hereby granted to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in
@@ -31,7 +29,7 @@ dealings in the software.
 PROGRAM DESCRIPTION
 -------------------
 Path optimization in the class definition of grid world. "Discovers"
-neighbors and assigns vertex numbers as they are discovered.
+neighbours and assigns vertex numbers as they are discovered.
 %}
 
 function obj = min_cost_path(obj)
@@ -42,14 +40,16 @@ isAllGoalSearch	= obj.searchSetup.reachAllGoals;
 idStart			= obj.searchSetup.start;
 idGoal			= obj.searchSetup.goal;
 
-%------ Cost, heuristic, and neighbor functions
-fcn_heuristic	= obj.searchHeuristicFcn;
-fcn_nhbr		= obj.neighborFcn;
+%------ Cost, heuristic, and neighbour functions
+% fcn_heuristic	= obj.searchHeuristicFcn;
+fcn_nhbr		= obj.neighbourFcn;
 
 nVertices		= 1E3;														% For large graphs, initialize search data structure with arbitrary size
-knownVertices	= zeros(nVertices, 1);
+knownVertices	= zeros(nVertices, 1);									
+% This is the list of known vertex IDs; the row number in this list is the
+% vertex number for that ID 
 
-% The assumption here is that the function that discovers neighboring
+% The assumption here is that the function that discovers neighbouring
 % vertices also provides a **unique** identifier to each vertex. The main
 % difference from a typical Dijkstra implementation is that the vertex ID
 % (which can be large) need not match this algorithm's internal indexing,
@@ -85,21 +85,21 @@ while (nFringe ~= 0) && (~isGoalClosed)
 	nFringe		= nFringe - 1;
 	fringe_(1, :) = [];	
 														
-	[nhbrID, nhbrCosts] = fcn_nhbr(vertexData(vCurrent).id);				% Other function handle for nhbrs and costs
+	[nhbrIDs, nhbrCosts] = fcn_nhbr(vertexData(vCurrent).id);				% Other function handle for nhbrs and costs
 	
-	for k = 1:numel(nhbrID)													% For all neighbors
+	for k = 1:numel(nhbrIDs)													% For all neighbours
 
-		if any(nhbrID(k) == knownVertices(1:nKnownVertices))
+		if any(nhbrIDs(k) == knownVertices(1:nKnownVertices))
 			tmp2		= 1:nKnownVertices;
-			vNeighbor	= tmp2(nhbrID(k) == knownVertices(1:nKnownVertices));
-			vNew		= vNeighbor;
+			vneighbour	= tmp2(nhbrIDs(k) == knownVertices(1:nKnownVertices));
+			vNew		= vneighbour;
 		else
 			vNew		= nKnownVertices + 1;
-			vertexData(vNew).id	= nhbrID(k);
+			vertexData(vNew).id	= nhbrIDs(k);
 			vertexData(vNew).mk	= 0;
 			
 			nKnownVertices		= vNew;
-			knownVertices(vNew)	= nhbrID(k);	
+			knownVertices(vNew)	= nhbrIDs(k);	
 		end
 		costNew	= nhbrCosts(k);												% Cost to go from act to new
 		
@@ -108,35 +108,18 @@ while (nFringe ~= 0) && (~isGoalClosed)
 		if vertexData(vNew).mk == 0											% Unvisited
 			vertexData(vNew).mk	= 1;										% Mark open
 			vertexData(vNew).d	= vertexData(vCurrent).d + costNew;			% Update c2come of newly visited state
-			vertexData(vNew).b	= vCurrent;
-			
+			vertexData(vNew).b	= vCurrent;			
 
-			tmp_open = bin_sort(fringe_(1:nFringe, :), [vNew vertexData(vNew).d], 2);
-% 			if numel(tmp_open) == 0
-% 				nFringe	= 0;
-% 				fringe_	= [];
-% 			else
-				nFringe	= size(tmp_open, 1);
-				fringe_(1:nFringe, :)	= tmp_open;							% Add [vertexNew cost] to sorted open list
-% 			end			
+			fringe_binary_sort([vNew vertexData(vNew).d]);					% Add [vertexNew cost] to sorted open list
 		elseif vertexData(vNew).mk == 1										% Already open, update c2come if necessary
 			if vertexData(vNew).d > vertexData(vCurrent).d + costNew
 				vertexData(vNew).d	= vertexData(vCurrent).d + costNew;
 				vertexData(vNew).b	= vCurrent;
 				
 				fringe_( (fringe_(1:nFringe, 1) == vNew), :) = [];
-				nFringe			= nFringe - 1;
+				nFringe	= nFringe - 1;
 				
-				tmp_open		= bin_sort(fringe_(1:nFringe, :), ...
-					[vNew vertexData(vNew).d], 2);					
-
-% 				if numel(tmp_open) == 0
-% 					nFringe		= 0;
-% 					fringe_	= [];
-% 				else
-					nFringe		= size(tmp_open, 1);
-					fringe_(1:nFringe, :)	= tmp_open;						% Add [vertexNew cost] to sorted open list
-% 				end
+				fringe_binary_sort([vNew vertexData(vNew).d]);				% Add [vertexNew cost] to sorted open list
 			end
 		end
 	end
@@ -144,16 +127,19 @@ while (nFringe ~= 0) && (~isGoalClosed)
 
 	if ~isAllGoalSearch
 		for k = 1:numel(idGoal)
-			[is_goal_known, idx_goal] = ismember(idGoal(k), knownVertices);
-			if is_goal_known && vertexData(idx_goal).mk == 2, isGoalClosed = 1; break; end
+			isGoalKnown		= (idGoal(k) == knownVertices);
+			if any(isGoalKnown) && vertexData(isGoalKnown).mk == 2
+				isGoalClosed = 1;
+				break;
+			end
 		end
 	else			
-		if ~all(ismember(idGoal, knownVertices)), continue; end
-		isGoalClosed = 1;
-		for k = 1:numel(idGoal)
-			[~, idx_goal] = ismember(idGoal(k), knownVertices);
-			if vertexData(idx_goal).mk == 2, isGoalClosed = 0; break; end
-		end
+% 		if ~all(ismember(idGoal, knownVertices)), continue; end
+% 		isGoalClosed = 1;
+% 		for k = 1:numel(idGoal)
+% 			[~, idx_goal] = ismember(idGoal(k), knownVertices);
+% 			if vertexData(idx_goal).mk == 2, isGoalClosed = 0; break; end
+% 		end
 	end
 
 end
@@ -165,6 +151,68 @@ obj.optimalPath = zeros(obj.nPoints, 1);
 obj.optimalPath(1:5) = 1:5;
 obj.pathCost	= 1;
 obj.pathRisk	= 2;
+
+
+	%======================================================================
+	function fringe_binary_sort(B)
+		% The rows of B are inserted into A, while sorting (ascending)
+		% according to column c. Both A and B have the same number of
+		% columns. A is assumed to sorted ascending.
+
+		A	= fringe_(1:nFringe, :);
+		c	= 2;
+		
+		[rA, cA] = size(A);
+		[rB, cB] = size(B);
+		
+		if numel(A) == 0
+			nFringe	= size(B, 1);
+			fringe_ = B;
+			return;
+		end
+		
+		if numel(B) == 0, return; end
+		if cB ~= cA, error('A and B must have same number of columns!\n'); end
+		
+		for m22 = 1:rB
+			thisIns		= B(m22, :);
+			thisCost	= thisIns(1, c);
+			
+			if ismember(thisIns, A, 'rows')
+				fprintf('This one came back!\t\t'); disp(thisIns)
+				redn = redn + 1;
+				continue;
+			end
+		
+			if A(rA, c) <= thisCost											% Insert after last row
+				A	= cat(1, A, thisIns);
+				rA	= rA + 1;
+				continue;
+			elseif A(1, c) >= thisCost										% Insert before first row
+				A	= cat(1, thisIns, A);
+				rA	= rA + 1;
+				continue;
+			end
+			
+			nCand	= rA;													% Number of candidate rows in A that can have greater cost
+			testRow	= 0;
+			dirn	= 1;	
+			while nCand > 1
+				p		= floor(nCand/2);
+				testRow = testRow + dirn*p;
+				dirn	= 2*(A(testRow, c) < thisCost) - 1;
+				nCand	= nCand - p;
+			end	
+		
+			insRow = testRow + (dirn + 1)/2;								% Insert before this row in A
+			A	= cat(1, A(1:(insRow - 1), :), thisIns, A(insRow:rA, :));
+			rA	= rA + 1;
+		end
+
+		nFringe					= size(A, 1);
+		fringe_(1:nFringe, :)	= A;
+	end
+
 
 end
 
